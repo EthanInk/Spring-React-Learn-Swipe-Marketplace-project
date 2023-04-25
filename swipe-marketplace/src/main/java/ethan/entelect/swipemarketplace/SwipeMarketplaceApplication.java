cb68c1a4bca520a681c098560f33c21f801202b1
EthanKlbEntelect <ethan.klb@entelect.co.za>
2023-04-25 09:10:57 +0200

    Spring init

---
+++ swipe-marketplace/src/main/java/ethan/entelect/swipemarketplace/SwipeMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package ethan.entelect.swipemarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipeMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipeMarketplaceApplication.class, args);
+	}
+
+}
---
+++ swipe-marketplace/src/test/java/ethan/entelect/swipemarketplace/SwipeMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ethan.entelect.swipemarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipeMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
